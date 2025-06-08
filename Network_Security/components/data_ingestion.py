597051a0a95a8f9ee9fb4ba97b0ee26d313c9a1e
itsRushikesh <rushi2001bobade@gmail.com>
2025-06-08 20:54:10 +0530

    Data -validation-transformation-modeltrainer.

--- Network_Security/components/data_ingestion.py
+++ Network_Security/components/data_ingestion.py
@@ -1,46 +1,51 @@
 from Network_Security.exception.exception import NetworkSecurityException
 from Network_Security.logging.logger import logging
-#data ingestion config
+
+
+## configuration of the Data Ingestion Config
+
 from Network_Security.entity.config_entity import DataIngestionConfig
 from Network_Security.entity.artifact_entity import DataIngestionArtifact
 import os
 import sys
+import numpy as np
+import pandas as pd
 import pymongo
 from typing import List
-import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 from dotenv import load_dotenv
 load_dotenv()
 
-MONGO_DB_URL = os.getenv("MONGO_DB_URL")
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
+
 
 class DataIngestion:
     def __init__(self,data_ingestion_config:DataIngestionConfig):
-        
         try:
             self.data_ingestion_config=data_ingestion_config
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
     def export_collection_as_dataframe(self):
-        """Read data from mongo db and return a dataframe"""
+        """
+        Read data from mongodb
+        """
         try:
-            database_name = self.data_ingestion_config.database_name
-            collection_name = self.data_ingestion_config.collection_name
-            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
-            collection = self.mongo_client[database_name][collection_name]
+            database_name=self.data_ingestion_config.database_name
+            collection_name=self.data_ingestion_config.collection_name
+            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
+            collection=self.mongo_client[database_name][collection_name]
+
+            df=pd.DataFrame(list(collection.find()))
+            if "_id" in df.columns.to_list():
+                df=df.drop(columns=["_id"],axis=1)
             
-            df = pd.DataFrame(list(collection.find()))
-            
-            if '_id' in df.columns.to_list():
-                df = df.drop(columns=['_id'],axis=1)
             df.replace({"na":np.nan},inplace=True)
             return df
         except Exception as e:
-            raise NetworkSecurityException(e,sys)
-    
-    def export_data_into_feature_store(self,dataframe:pd.DataFrame):
+            raise NetworkSecurityException
+        
+    def export_data_into_feature_store(self,dataframe: pd.DataFrame):
         try:
             feature_store_file_path=self.data_ingestion_config.feature_store_file_path
             #creating folder
@@ -51,7 +56,7 @@ class DataIngestion:
             
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-    
+        
     def split_data_as_train_test(self,dataframe: pd.DataFrame):
         try:
             train_set, test_set = train_test_split(
@@ -93,4 +98,4 @@ class DataIngestion:
             return dataingestionartifact
 
         except Exception as e:
-            raise NetworkSecurityException
+            raise NetworkSecurityException(e,sys)
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from Network_Security.components.data_ingestion import DataIngestion
+from Network_Security.components.data_validation import DataValidation
+from Network_Security.components.data_transformation import DataTransformation
 from Network_Security.exception.exception import NetworkSecurityException
 from Network_Security.logging.logger import logging
-from Network_Security.entity.config_entity import DataIngestionConfig
+from Network_Security.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from Network_Security.entity.config_entity import TrainingPipelineConfig
 
-
+from Network_Security.components.model_trainer import ModelTrainer
+from Network_Security.entity.config_entity import ModelTrainerConfig
+ 
 
 import sys
 
@@ -17,5 +21,24 @@ if __name__=='__main__':
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
         print(dataingestionartifact)
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print(data_validation_artifact)
+        # data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        # logging.info("data Transformation started")
+        # data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        # data_transformation_artifact=data_transformation.initiate_data_transformation()
+        # print(data_transformation_artifact)
+        # logging.info("data Transformation completed")
+
+        # logging.info("Model Training sstared")
+        # model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        # model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        # model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
     except Exception as e:
         raise NetworkSecurityException(e,sys)
